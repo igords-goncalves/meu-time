1ecfe5b232e446336ad736d1b2139256d0dd46a7
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-19 07:23:49 -0300

    feat: criar componente section

--- src/components/templates/Section/index.tsx
+++ src/components/templates/Section/index.tsx
@@ -1,7 +1,25 @@
+import warming from '../../../assets/img/icon-warming.svg';
+import './style.scss';
+
 export const Section = () => {
   return (
-    <div>
-      <h1>Section</h1>
-    </div>
+    <section className="c-section">
+      <div className="u-container">
+        <div className="c-section__login">
+          <h1>Login Card</h1>
+        </div>
+        <div className="c-section__text">
+          <img src={warming} alt="" className="c-section__img" />
+          <p className="c-section__paragraph">
+            Para efetuar o login você deve criar uma conta em API-Football
+            antes.{' '}
+            <span className="u-strong">
+              Após criar a conta você receberá uma chave de autenticação,
+              preencha o campo acima com sua chave e faça login.
+            </span>
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
