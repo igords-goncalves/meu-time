51a103b441f089aa36e3a964c037d78b22b2f5c7
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-19 09:10:15 -0300

    fix: passar propriedade style por props para o componente botão

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -3,11 +3,12 @@ import './style.scss';
 interface PropsButton {
   label: string;
   onClick: () => void;
+  style?: object;
 }
 
-export const Button = ({ onClick, label }: PropsButton) => {
+export const Button = ({ onClick, label, style }: PropsButton) => {
   return (
-    <button className="c-button" onClick={onClick}>
+    <button className="c-button" onClick={onClick} style={style}>
       {label}
     </button>
   );
