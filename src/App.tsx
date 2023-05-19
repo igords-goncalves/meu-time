26b0c70625d2507973970fe57864184d3dac344c
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-19 18:47:43 -0300

    feat: inserir toastcontainer em app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import './App.scss';
+import 'react-toastify/dist/ReactToastify.css';
 import { Grid } from './components/layout/Grid';
 import { Footer } from './components/templates/Footer';
 import { Header } from './components/templates/Header';
 import { Section } from './components/templates/Section';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <Grid>
-      <Header />
-      <Section />
-      <Footer />
-    </Grid>
+    <>
+      <Grid>
+        <Header />
+        <Section />
+        <Footer />
+      </Grid>
+      <ToastContainer />
+    </>
   );
 }
 
