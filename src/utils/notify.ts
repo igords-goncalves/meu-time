e544dd0ad2cf70ba46996e8941205c5c94ce4485
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-20 16:22:49 -0300

    refactor: incluir pop-up de erro caso algo saia errado no check in

--- src/utils/notify.ts
+++ src/utils/notify.ts
@@ -1,8 +1,18 @@
 import { toast } from 'react-toastify';
 
-export const notify = (data: any) => {
+export const success = (data: any) => {
   toast.success(
     `Login efetuado com sucesso ${data.response.account.firstname}! `,
+    {
+      position: 'top-right',
+      theme: 'light',
+    },
+  );
+};
+
+export const err = () => {
+  toast.error(
+    'Desculpe algo saiu errado verifique sua chave de acesso e tente novamente',
     {
       position: 'top-right',
       theme: 'light',
