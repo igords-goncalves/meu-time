de08a6fd77871b670503339ed58473fbf0e681bb
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-20 15:39:42 -0300

    feat: criar pagina Login e importar componentes

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import { Footer } from '../../components/templates/Footer';
+import { Header } from '../../components/templates/Header';
+import { Section } from '../../components/templates/Section';
+
+export const Login = () => {
+  return (
+    <>
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  );
+};
