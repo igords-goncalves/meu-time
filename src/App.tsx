673d00485bb0d85f7f34402c79e5c39b17f01982
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-20 15:38:42 -0300

    feat: importar componente page dentro de app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import './App.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import { Grid } from './components/layout/Grid';
-import { Footer } from './components/templates/Footer';
-import { Header } from './components/templates/Header';
-import { Section } from './components/templates/Section';
+import { Login } from './pages/Login';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
       <Grid>
-        <Header />
-        <Section />
-        <Footer />
+        <Login />
       </Grid>
       <ToastContainer />
     </>
