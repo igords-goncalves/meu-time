132c3b6c157bd1329a835f5b200c5c730a3a2466
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-20 22:10:44 -0300

    feat: inserir arquivos de rotas no componente app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './App.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import { Grid } from './components/layout/Grid';
-import { Login } from './pages/Login';
 import { ToastContainer } from 'react-toastify';
+import { MainRoutes } from './router/routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <Grid>
-        <Login />
+        <MainRoutes />
       </Grid>
       <ToastContainer />
-    </>
+    </Router>
   );
 }
 
