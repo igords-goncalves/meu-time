f48a047c35b4d41063482d99e4a7782ed7bcf2c5
Igor Gonçalves <igordossantos38@gmail.com>
2023-05-20 22:12:30 -0300

    feat: criar pagina Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Footer } from '../../components/templates/Footer';
+import { Header } from '../../components/templates/Header';
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>Home</h1>
+      <Footer />
+    </>
+  );
+};
