cc4611698b55102b5822f9cd85e0cba7c5fc341d
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-12 23:52:35 -0300

    feat: create route to page team

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { Login } from '../pages/Login';
 import { Home } from '../pages/Home';
+import { Team } from '../pages/Team';
 
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/team" element={<Team />} />
     </Routes>
   );
 };
