a41202284e9df4c3710c3b5d6c16fdbf6d320be8
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-12 23:51:39 -0300

    feat: create page team

---
+++ src/pages/Team/index.tsx
@@ -0,0 +1,12 @@
+import { Footer } from '../../components/templates/Footer';
+import { Header } from '../../components/templates/Header';
+
+export const Team = () => {
+  return (
+    <>
+      <Header />
+      <h1>Team</h1>
+      <Footer />
+    </>
+  );
+};
