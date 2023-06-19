74dcd4c0594726e0ac5248b7cce6a31fbc384a67
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-19 15:23:40 -0300

    refactor: change layout of header and insert a nav area

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,26 +1,39 @@
-import { Button } from '../../common/Button';
-
 import './style.scss';
 import logoName from '../../../assets/img/logo-name-small.svg';
 import logoFullBig from '../../../assets/img/logo-full-big.svg';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="c-header">
-      <div className="c-header__menu u-container">
-        <img
-          src={logoName}
-          alt="Logomarca meu time"
-          className="c-header__logo"
-        />
-        <Button label="Login" onClick={() => console.log('teste')} />
-      </div>
       <div className="c-header__banner">
+        <nav className="c-header__nav u-container">
+          <img
+            src={logoName}
+            alt="Logomarca meu time"
+            className="c-header__logo"
+            onClick={() => navigate('/')}
+          />
+          <ul className="c-header__menu">
+            <li className="c-header__item">
+              <Link className="c-header__link" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="c-header__item">
+              <Link className="c-header__link" to="/dashboard">
+                Dashboard
+              </Link>
+            </li>
+          </ul>
+        </nav>
         <div className="u-container">
           <img
             src={logoFullBig}
             alt="Logomarca meu time"
-            className="c-header__logo"
+            className="c-header__logo-banner"
           />
           <p className="c-header__text">
             Informações sobre ligas, times, temporadas, estatísticas e jogadores
