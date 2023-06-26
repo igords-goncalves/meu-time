0febccddeb63ec14850c6c1dc14dd44d0cfebe2c
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-26 08:29:41 -0300

    feat: reference elements and child components on home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,36 @@
+import { Countries } from '../../components/Countries';
+import { Leagues } from '../../components/Leagues';
 import { Footer } from '../../components/templates/Footer';
-import { Header } from '../../components/templates/Header';
+import { NavBar } from '../../components/templates/NavBar';
+import './style.scss';
 
 export const Home = () => {
   return (
     <>
-      <Header />
-      <h1>Home</h1>
+      <NavBar />
+      <section className="section__countries">
+        <div className="u-container">
+          <div className="section__wrapper">
+            <h2 className="section__title">Países</h2>
+            <p className="section__text">Selecione um país abaixo.</p>
+          </div>
+          <Countries />
+        </div>
+      </section>
+      <hr />
+      <section className="section__leagues">
+        <div className="u-container">
+          <div className="section__wrapper">
+            <h2 className="section__title">Principais Ligas</h2>
+            <p className="section__text">
+              Selecione uma liga na lista defina uma temporada opcional e
+              escolha uma time, para ver todas as informações do time escolhido,
+              click no botão ver.
+            </p>
+          </div>
+          <Leagues />
+        </div>
+      </section>
       <Footer />
     </>
   );
