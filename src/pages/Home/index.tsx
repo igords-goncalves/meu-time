4c6a60962f4528b9d8dcce4728f7f2671c82d286
Igor Gonçalves <igordossantos38@gmail.com>
2023-06-26 11:09:59 -0300

    feat: create a request for countries

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,22 @@
-import { Countries } from '../../components/Countries';
+import { useEffect, useState } from 'react';
+import { Slide } from '../../components/Slide';
 import { Leagues } from '../../components/Leagues';
 import { Footer } from '../../components/templates/Footer';
 import { NavBar } from '../../components/templates/NavBar';
 import './style.scss';
 
+import { useApi } from '../../hooks/useApi';
+
 export const Home = () => {
+  const api = useApi();
+
+  async function handleCountries() {
+    const res = await api.getCountries();
+    const country = res.response;
+    return country;
+  }
+  handleCountries();
+
   return (
     <>
       <NavBar />
@@ -14,7 +26,7 @@ export const Home = () => {
             <h2 className="section__title">Países</h2>
             <p className="section__text">Selecione um país abaixo.</p>
           </div>
-          <Countries />
+          <Slide />
         </div>
       </section>
       <hr />
