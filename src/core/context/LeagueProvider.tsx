e89405f6cfba0b9ccf1df5b8b7b5a64947616fbf
Igor Gonçalves <igordossantos38@gmail.com>
2026-04-09 08:10:44 -0300

    refactor: use useMemo for LeagueContext value and update leagues type

--- src/core/context/LeagueProvider.tsx
+++ src/core/context/LeagueProvider.tsx
@@ -1,28 +1,27 @@
-import { ReactNode, useState } from 'react';
-import { LeagueContext } from './LeagueContext';
+import { ReactNode, useMemo, useState } from 'react';
+import { League, LeagueContext } from './LeagueContext';
 
 export const LeagueProvider = ({ children }: { children: ReactNode }) => {
-  const [leagues, setLeagues] = useState<[] | null>(null);
+  const [leagues, setLeagues] = useState<League[] | null>(null);
   const [season, setSeason] = useState<number | null>(null);
-
   const [selectedLeague, setSelectedLeague] = useState<number | null>(null);
   const [selectedSeason, setSelectedSeason] = useState<number | null>(null);
 
+  const value = useMemo(
+    () => ({
+      leagues,
+      setLeagues,
+      season,
+      setSeason,
+      // to render teams
+      selectedLeague,
+      setSelectedLeague,
+      selectedSeason,
+      setSelectedSeason,
+    }),
+    [leagues, season, selectedLeague, selectedSeason],
+  );
   return (
-    <LeagueContext.Provider
-      value={{
-        leagues,
-        setLeagues,
-        season,
-        setSeason,
-        // to render teams
-        selectedLeague,
-        setSelectedLeague,
-        selectedSeason,
-        setSelectedSeason,
-      }}
-    >
-      {children}
-    </LeagueContext.Provider>
+    <LeagueContext.Provider value={value}>{children}</LeagueContext.Provider>
   );
 };
